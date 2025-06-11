af886210507af2bf0154cce0a03fa8afb744d1d5
Galaxy <subhamgalaxy6969@gmail.com>
2025-06-11 18:14:37 +0530

    Add files via upload

---
+++ simple_programng.py
@@ -0,0 +1,10 @@
+a=float(input('Enter the first number: '))
+b=float(input('Enter the second number: '))
+c= a + b
+print("Addition: ",c)
+d= a - b
+print("substract: ",d)
+e= a * b
+print("multiplication: ",e)
+f= a / b
+print("devision: ",f)
